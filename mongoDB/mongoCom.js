56e554049f30f1d3fb37ce3d9ca952dfa938be63
xbbraveknight <github.com/xbbraveknight>
2022-11-18 16:33:46 -0300

    "mongo changes"

--- mongoDB/mongoCom.js
+++ mongoDB/mongoCom.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGO_CONNECT, ()=>{
+mongoose.connect(process.env.PORT || 3001, '0.0.0.0', ()=>{
     console.log('Conectado!')
-    
+
 })
