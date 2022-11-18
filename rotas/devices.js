a91b77e21eb5a7da1e2506783133e26718f54341
xbbraveknight <github.com/xbbraveknight>
2022-11-18 16:39:59 -0300

    "heroku"

--- rotas/devices.js
+++ rotas/devices.js
@@ -44,6 +44,7 @@ router.post('/', async (req, res)=>{
    
 })
 
+//atualizar dispositivos
 router.put('/:id', async (req, res)=>{
     try{
         const updateDeviceId = await Device.updateOne(
--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express')
 const bodyParser = require('body-parser')
 require('dotenv').config()
 const app = express()
-
 app.use(bodyParser.json())
+
 const api = require('./rotas/')
 app.use('/api', api)
-const PORT = 3000
+const PORT = process.env.PORT
 
 
 app.get('/', (req, res)=>{
