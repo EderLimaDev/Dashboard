40747ab2e8ea1aa6910670951604157637624b9a
xbbraveknight <github.com/xbbraveknight>
2022-11-23 23:11:33 -0300

    "correcao de varios bugs e operacoes"

--- frontend/src/componentes/admin/AdmDevice.js
+++ frontend/src/componentes/admin/AdmDevice.js
@@ -3,7 +3,7 @@ import styled from 'styled-components'
 import {Container, Table, Image, Button} from "react-bootstrap"
 import {useApi} from '../../hooks/useApi'
 import moment from 'moment'
-import caixaDialogo from './caixaDialogo'
+import CaixaDialogo from './caixaDialogo'
 import FormEdit from './formEdit'
 import {deleteRegistro, editRegistro} from '../../services/api'
 
@@ -58,11 +58,11 @@ const AdmDevice = ()=>{
         variant: '',
         label: '',
         body: '',
-        email: '',
+        email: ''
     })
     
-    const manipulaShow = (device, esc)=>{
-        setEscAtual(esc)
+    const manipulaShow = (device, escolha)=>{
+        setEscAtual(escolha)
         setShow(true)
         setId(device._id)
         setEmail(device.email)
@@ -82,51 +82,52 @@ const AdmDevice = ()=>{
 
     return(
         <Container id='root'>
-            <Table striped bordered hover variant="dark">
-                <thead>
-                    <tr>
-                        <th><H5>Foto</H5></th>
-                        <th><H5>Device ID</H5></th>
-                        <th><H5>Nome</H5></th>
-                        <th><H5>Data de cadastro</H5></th>
-                        <th><H5>Ações</H5></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {data?.message?.map(projeto =>{
-                        return(<tr>
-                                <td><Foto src={projeto.imagem} alt="imagem" /></td>
-                                <td>{projeto._id}</td>
-                                <td>{projeto.nome}</td>
-                                <td>{moment(projeto.data).format('DD-MM-YYYY')}</td>
-                                <td>
-                                    <Button variant='info' onClick={()=> manipulaShow(projeto, escolha.edit)}>Editar</Button>&nbsp;&nbsp;
-                                    <Button variant='danger' onClick={()=> manipulaShow(projeto, escolha.delete)}>Deletar</Button>
-                                </td>
-                            </tr>)
-                    })}
-                </tbody>
+                <Table striped bordered hover variant="dark">
+                    <thead>
+                        <tr>
+                            <th><H5>Foto</H5></th>
+                            <th><H5>Device ID</H5></th>
+                            <th><H5>Nome</H5></th>
+                            <th><H5>Data de cadastro</H5></th>
+                            <th><H5>Ações</H5></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {data?.message?.map(projeto =>{
+                            return(<tr>
+                                    <td><Foto src={projeto.imagem} thumbnail /></td>
+                                    <td>{projeto._id}</td>
+                                    <td>{projeto.nome}</td>
+                                    <td>{moment(projeto.data).format('DD-MM-YYYY')}</td>
+                                    <td>
+                                        <Button variant='info' onClick={()=> manipulaShow(projeto, escolha.edit)}>Editar</Button>&nbsp;&nbsp;
+                                        <Button variant='danger' onClick={()=> manipulaShow(projeto, escolha.delete)}>Deletar</Button>
+                                    </td>
+                                </tr>)
+                        })}
+                    </tbody>
 
-            </Table>
+                </Table>
 
-            <caixaDialogo show={show} setShow={setShow} escAtual={escAtual} id={id} email={email}>
-                    {escAtual.mostraBody && escAtual.body}
+            <CaixaDialogo show={show} setShow={setShow} escAtual={escAtual} id={id} email={email}>
+                {escAtual.mostraBody && escAtual.body}
 
-                    {!escAtual.mostraBody && (
-                        <FormEdit 
-                            nome={nome} 
-                            setNome={setNome} 
-                            id={id}
-                            setId={setId}
-                            email={email}
-                            setEmail={setEmail}
-                            descricao={descricao} 
-                            setdescricao={setDescricao} 
-                            imagem={imagem} 
-                            setImagem={setImagem}
+                {!escAtual.mostraBody && (
+                    <FormEdit 
+                        nome={nome} 
+                        setNome={setNome} 
+                        id={id}
+                        setId={setId}
+                        email={email}
+                        setEmail={setEmail}
+                        descricao={descricao} 
+                        setdescricao={setDescricao} 
+                        imagem={imagem} 
+                        setImagem={setImagem}
                         />
                     )}
-                </caixaDialogo>
+
+            </CaixaDialogo>
         </Container>
     )
 }
@@ -190,141 +191,3 @@ export default  AdmDevice
 
 
 
-
-// import React, {useState} from 'react'
-// import {Container, Table, Image, Button} from 'react-bootstrap'
-// import styled from 'styled-components'
-// import moment from 'moment'
-// import {useApi} from '../../hooks/useApi'
-// import FormEdit from './formEdit'
-// import { editRegistro, deleteRegistro } from '../../services/api'
-// import CaixaDialogo from './caixaDialogo'
-
-// const Foto = styled(Image)`
-//     height: 100px;
-// `;
-
-// const AdmDevice = ()=> { 
-
-//     const del = (id)=>{
-//         deleteRegistro(id)
-//         window.location.reload();
-//     }
-
-//     const ed = (id, data)=>{
-//         editRegDevice(id, data)
-//         window.location.reload();
-//     }
-
-//     const [escolha] = useState({
-//         delete:{
-//             header:'Deletar Dispositivo?',
-//             variant:'danger',
-//             label:'OK',
-//             mostraBody:true,
-//             body:'Tem certeza que deseja deletar o registro?',
-//             callback: del
-//         }, 
-//         edit:{
-//             header:'Editar Dispositivo?',
-//             variant:'primary',
-//             label:'Salvar',
-//             mostraBody: false,
-//             callback: ed
-           
-
-//         }
-//     })
-
-  
-//     const [nome, setNome] = useState()
-//     const [descricao, setDescricao] = useState()
-//     const [imagem, setImagem] = useState()
-//     const [id, setId] = useState()
-//     const [email, setEmail] = useState()
-//     const [show, setShow] = useState(false)
-//     const {data} = useApi('/devices')
-
-//     const [escolhaAtual, setEscolhaAtual] = useState({
-//         header: '',
-//         variant: '',
-//         label:'',
-//         body:'',
-//         id:'',
-//         email:''
-//     })
-
-//     const manipulaShow = (device, escolha)=>{
-//         setEscolhaAtual(escolha)
-//         setShow(true)
-//         setId(device._id)
-//         setEmail(device.email)
-//         setNome(device.nome)
-//         setDescricao(device.descricao)
-//         setImagem(device.imagem)
-//     }
-
-//     const editRegDevice = (id,data)=>{
-//         const newRegDevice = {
-//             nome: data.nome,
-//             descricao: data.descricao,
-//             imagem: data.imagem
-//         }
-//         editRegistro(id, newRegDevice)
-//     }
-
-
-
-//     return (
-//     <Container>
-       
-//             <Table striped bordered hover variant="dark">
-//                     <thead>
-//                         <tr>
-//                             <th>Foto</th>
-//                             <th>ID Dispositivo</th>
-//                             <th>Nome</th>
-//                             <th>Data do Cadastro</th>
-//                             <th>Ações</th>
-//                         </tr>
-//                     </thead>
-//                     <tbody>
-//                     {data?.message?.map(projeto =>{
-//                         return(
-//                             <tr>
-//                                 <td><Foto src={projeto.imagem} alt='imagem'></Foto></td>
-//                                 <td>{projeto._id}</td>
-//                                 <td>{projeto.nome}</td>
-//                                 <td>{moment(projeto.data).format('DD-MM-YYYY')}</td>
-//                                 <td>
-//                                     <Button variant="info" onClick={()=> manipulaShow(projeto,escolha.edit)}>Editar</Button>&nbsp;&nbsp;
-//                                     <Button variant="danger" onClick={()=> manipulaShow(projeto,escolha.delete)}>Deletar</Button>
-//                                 </td>
-//                             </tr>)
-//                     })}
-                        
-//                     </tbody>
-//                 </Table>
-//                <CaixaDialogo show={show} setShow={setShow} escolhaAtual={escolhaAtual} id={id} email={email} > 
-//                     {escolhaAtual.mostraBody && escolhaAtual.body}
-                
-//                     {!escolhaAtual.mostraBody && (
-//                         <FormEdit
-//                         nome={nome}
-//                         setNome={setNome}
-//                         id={id}
-//                         setId={setId}
-//                         email={email}
-//                         setEmail={setEmail}
-//                         descricao={descricao}
-//                         setDescricao={setDescricao}
-//                         imagem={imagem}
-//                         setImagem={setImagem}
-
-//                         />
-//                     )}
-//                </CaixaDialogo>             
-//     </Container>
-//   )
-// }
-// export default AdmDevice
--- frontend/src/componentes/admin/caixaDialogo.js
+++ frontend/src/componentes/admin/caixaDialogo.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import {Button, Modal, ModalHeader, ModalBody, ModalFooter, ModalTitle} from 'react-bootstrap'
 
-function caixaDialogo(show, setShow, escolhaAtual, id, email, children) {
+const CaixaDialogo = ({show, setShow, escAtual, id, email, children})=> {
     const acao = ()=>{
-        escolhaAtual.callback(id, children[1].props)
+        escAtual.callback(id, children[1].props)
         setShow(false)
 
     }
+
     return (    
             <Modal
                 show={show}
                 onHide={()=> setShow(false)}
+                backdrop="static"
+                keyboard={false}
                 >
 
-                <ModalHeader closeButton>
-                    <ModalTitle>{escolhaAtual.header}<br/>
-                                {id}<br/>
-                                {email}<br/>   
-                    </ModalTitle>
-                </ModalHeader> 
+                    <ModalHeader closeButton>
+                        <ModalTitle>{escAtual.header}<br/>
+                                    {id}<br/>
+                                    {email}<br/>   
+                        </ModalTitle>
+                    </ModalHeader> 
 
-                <ModalBody>
-                    {children}
-                </ModalBody>
+                    <ModalBody>
+                        {children}
+                    </ModalBody>
 
                 <ModalFooter>
-                    <Button variant={escolhaAtual.variant} onClick={()=> setShow(false)}>Cancelar</Button>
-                    <Button variant={escolhaAtual.variant} onClick={()=> acao()}>{escolhaAtual.label}</Button>
+                    <Button variant={escAtual.variant} onClick={()=> setShow(false)}>Cancelar</Button>
+                    <Button variant={escAtual.variant} onClick={()=> acao()}>{escAtual.label}</Button>
 
                 </ModalFooter>
                 
             </Modal>
-    );
+    )
 }
 
-export default caixaDialogo;
+export default CaixaDialogo
